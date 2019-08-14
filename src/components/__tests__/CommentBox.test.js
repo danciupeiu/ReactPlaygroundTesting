08a65a9db4b0646f317f342b342abda0d0350734
Dan Ciupeiu <ciupeiu_dan@yahoo.com>
2019-08-14 22:24:40 +0200

    Added test for textarea value change and for form submit

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -5,15 +5,29 @@ import CommentBox from 'components/CommentBox';
 
 let wrapped;
 beforeEach(() => {
-    wrapped = mount(<CommentBox />);
+    wrapped = mount(<CommentBox/>);
 });
 
 afterEach(() => {
     wrapped.unmount();
 });
 
-it ('has a text area and a button', () => {
+it('has a text area and a button', () => {
     expect(wrapped.find('textarea').length).toEqual(1);
     expect(wrapped.find('button').length).toEqual(1);
 });
 
+it('allows input on the text area', () => {
+    const val = 'somevalue';
+    wrapped.find('textarea').simulate('change', {target: {value: val}});
+    wrapped.update();
+    expect(wrapped.find('textarea').prop('value')).toEqual(val);
+});
+
+it ('allows submit and clears the text area', () => {
+    const val = 'val';
+    wrapped.find('textarea').simulate('change', { target: { value: val}});
+    wrapped.find('form').simulate('submit');
+    wrapped.update();
+    expect(wrapped.find('textarea').prop('value')).toEqual('');
+});
