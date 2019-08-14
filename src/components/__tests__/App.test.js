b9d1e1d70dc2b11af06c484d0191ede781a87523
Dan Ciupeiu <dan.ciupeiu@jpard.com>
2019-08-14 09:05:21 +0300

    Added beforeEach

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import {shallow} from 'enzyme';
 
 import CommentBox from 'components/CommentBox';
 import CommentList from 'components/CommentList';
 import App from 'components/App';
 
-it('shows the comment box', () => {
-    const wrapped = shallow(<App/>);
+let wrapped;
 
+beforeEach(() => {
+    wrapped = shallow(<App/>);
+});
+
+it('shows the comment box', () => {
     expect(wrapped.find(CommentBox).length).toEqual(1);
 });
 
 it('shows the comment list', () => {
-    const wrapped = shallow(<App/>);
-
     expect(wrapped.find(CommentList).length).toEqual(1);
 });
